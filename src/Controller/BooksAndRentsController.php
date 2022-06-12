c75fa2c3f995030c328d956121abfb46970e4f9f
unknown <arbreplantebuisson@gmail.com>
2022-06-12 21:36:08 +0200

    'ok'

--- src/Controller/BooksAndRentsController.php
+++ src/Controller/BooksAndRentsController.php
@@ -37,7 +37,13 @@ class BooksAndRentsController extends AbstractFOSRestController
     #[Route('/books/ISBN/{ISBN}', name: 'app_books_and_rents_ISBN', methods: ['GET', 'POST'])]
     public function findByISBN($ISBN, BooksAndRentsRepository $repo): Response
     {
-        return $this->json(['BooksAll'=>$repo->findByISBN($ISBN),Response::HTTP_ACCEPTED]);
+        $BOOKS=$repo->findByISBN($ISBN);
+        if ($BOOKS===[]){
+            return $this->json(['BooksAll'=>"no books",Response::HTTP_NO_CONTENT]);
+        }
+        else{
+            return $this->json(['BooksAll'=>$BOOKS,Response::HTTP_ACCEPTED]);
+        }
     }
 
     //  // #[Route('/create/location', name: 'answer_question', methods: ['GET', 'POST'])]
@@ -64,6 +70,72 @@ class BooksAndRentsController extends AbstractFOSRestController
         }
         $em->persist($BooksAndRents);
          $em->flush();
+        return $this->view("| Response :".Response::HTTP_CREATED." |");
+    }
+
+
+    
+    /**
+     * @Rest\Post("/api/searchBOOKS/{ISBN}", name="search_books")
+     * @Rest\View()
+     */
+    public function createBooksAndRents($ISBN,ManagerRegistry $doctrine){
+        $curl = curl_init();
+        $em = $doctrine->getManager();
+        curl_setopt_array($curl, [
+            CURLOPT_URL => "https://www.googleapis.com/books/v1/volumes?q=isbn:".$ISBN,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_SSL_VERIFYPEER=> 0,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CONNECTTIMEOUT=> 30,
+            CURLOPT_POST=> true,
+            CURLOPT_CUSTOMREQUEST => "GET"
+        ]);
+
+        $response = curl_exec($curl);
+        $response = json_decode($response,true);
+        
+        curl_close($curl);
+        $response=$response["items"];
+        // return $response;
+        foreach ($response as $bookAPI) {
+            $bookAndRents = new BooksAndRents;
+            $bookAndRents->setIsbn($bookAPI["volumeInfo"]["industryIdentifiers"][0]["identifier"]);
+            if(isset($bookAPI["volumeInfo"]["authors"][0]))
+            {
+                $bookAndRents->setAuthor($bookAPI["volumeInfo"]["authors"][0]);
+            }
+           
+            $bookAndRents->setTitle($bookAPI["volumeInfo"]["title"]);
+
+            if(isset($bookAPI["volumeInfo"]["publisher"]))
+                {
+                    $bookAndRents->setPublisher($bookAPI["volumeInfo"]["publisher"]);
+                }
+
+                if(isset($bookAPI["volumeInfo"]["categories"]))
+                {
+                    $bookAndRents->setCategory($bookAPI["volumeInfo"]["categories"][0]);
+                }
+                if(isset($bookAPI["volumeInfo"]["description"]))
+                {
+                    $bookAndRents->setDescription($bookAPI["volumeInfo"]["description"]);
+                }
+                if(isset($bookAPI["volumeInfo"]["imageLinks"]["smallThumbnail"]))
+                {
+                    $bookAndRents->setCover($bookAPI["volumeInfo"]["imageLinks"]["smallThumbnail"]);
+                }
+         
+            $bookAndRents->setLanguage($bookAPI["volumeInfo"]["language"]);
+            $em->persist($bookAndRents);
+          
+        }
+        $em->flush();
+
         return $this->view("| Response :".Response::HTTP_CREATED." |");
     }
 }
